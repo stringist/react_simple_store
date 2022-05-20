5f216eb2d2e4acb10388d6f5e74a76a56f25e6d4
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-05-20 09:57:43 +0200

    form works

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,8 +1,18 @@
+import {useRef} from "react";
 export default function CheckoutForm() {
+const formEl = useRef(null);
+function submitted(e) {
+e.preventDefault();
+console.log(formEl.current.elements.fullname.value);
+console.log(formEl.current.elements.address.value);}
   return (
     <div>
-      <form>
-<input type="text" /></form>
+      <form ref={formEl} onSubmit={submitted}>
+<label htmlFor="fullname">Full name</label>
+<input type="text" name="fullname" id="fullname" required/>
+<label htmlFor="address">Address</label>
+<input type="text" name="address" id="address" required/>
+<button>Pay</button></form>
     </div>
   );
 }
